1b38fe206fdac7bf5fd604b2a761ad28335cd1d2
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-08-24 12:04:12 -0400

    Still working on the campaign routes and migrations.5

--- app/Http/Controllers/CampaignController.js
+++ app/Http/Controllers/CampaignController.js
@@ -5,12 +5,12 @@ const Campaign = use('App/Model/Campaigns')
 class CampaignController {
 
 	* createCampaign (req, resp){
-		const user  = request.authUser
+		const user  = req.authUser
 		const input = req.only('title', 'description') 
 		const campaign = yield Campaign.create(input)
 		console.log(input)
 		yield user.campaigns().save(campaign)
-        return response.json(campaign);
+        return resp.json(campaign);
 	}
 
 	
